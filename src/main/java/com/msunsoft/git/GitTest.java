4e6935f844080f1afb394577e53b87455452580e
liwenzhuo <liwenzhuo@msunsoft.com>
2022-05-30 22:29:17 +0800

    first commit

---
+++ src/main/java/com/msunsoft/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.msunsoft.git;
+
+/**
+ * @create: 2022/5/30
+ * @Description:
+ * @FileName: GitTest
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
+
+
